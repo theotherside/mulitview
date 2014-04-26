1084accb06267256b754ad9eacb3329472f9b2da
Luke <luke.monson@me.com>
2014-04-26 17:31:40 -0500

    Initial Commit

---
+++ Multiview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiview
+//
+//  Created by lrm on 4/26/14.
+//  Copyright (c) 2014 Monson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multiview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiview
+//
+//  Created by lrm on 4/26/14.
+//  Copyright (c) 2014 Monson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
